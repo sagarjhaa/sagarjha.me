2bd472a71fc2a6c0dbe687c2cc19cb22e2d156e5
Sagar <sagar.jhaa@gmail.com>
2019-10-06 20:58:57 -0700

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,112 @@
+import React, { useState } from "react"
 import { graphql, Link, StaticQuery } from "gatsby"
-import React from "react"
+import { withStyles } from "@material-ui/core/styles"
+import Switch from "@material-ui/core/Switch"
+
+// import Logo from "../assets/images/logo.svg"
+
+// import useDarkMode from "../hooks/src/darkmode"
 
 import "./header.scss"
 
-const Header = () => (
+const DarkSwitch = withStyles({
+  switchBase: {
+    color: "#6772e5",
+    "&$checked": {
+      color: "#6772e5",
+      "&:hover": {
+        backgroundColor: "rgba(255, 255, 255, 0.08)",
+      },
+    },
+    "&$checked + $track": {
+      backgroundColor: "#fff",
+      opacity: 1,
+    },
+  },
+  checked: {},
+  track: {
+    backgroundColor: "#252525",
+    opacity: 1,
+  },
+})(Switch)
+
+const Header = () => {
+  const [isNavbarOpen, setNavbarOpen] = useState(false)
+//   const [isDarkMode, setDarkMode] = useDarkMode("dark")
+
+  return (
     <StaticQuery
-        query={graphql`
-            query PageInfoQuery {
-                allFile(filter: { sourceInstanceName: { eq: "pages" } }) {
-                    edges {
-                        node {
-                            name
-                            id
-                        }
-                    }
-                }
+      query={graphql`
+        {
+          allFile(
+            filter: {
+              sourceInstanceName: { eq: "pages" }
+              extension: { eq: "js" }
             }
-        `}
-      render={data => {
-        return (
-          <header>
-              <div
-                  style={{
-                      margin: `0 auto`,
-                      maxWidth: 960,
-                      padding: `1.45rem 1.0875rem`,
-                  }}
-              >
-                  <nav>
-                      {data.allFile.edges
-                          .filter(edge => {
-                              return edge.node.name !== "404" 
-                          })
-                          .map(edge => {
-                              let link = "/";
-                              let name = "Home"
-                              if (edge.node.name !== "index") {
-                                  link += edge.node.name;
-                                  name = edge.node.name.charAt(0).toUpperCase() + edge.node.name.slice(1);
-                              }
-                              return (
-                                  <Link key={edge.node.id} to={link} className="navbar-item">{name}</Link>
-                              )
-                          }
-                      )}
-                  </nav>
-              </div>
-          </header>
-      )
-      }}
+          ) {
+            edges {
+              node {
+                name
+                id
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <header>
+          <div className="brand">
+            <Link to={"/"} aria-label="Brand Logo" className="brand__logo">
+              {/* <Logo /> */}
+            </Link>
+            <span className="brand__name">Sagar Jha</span>
+          </div>
+          <div
+            onClick={() => setNavbarOpen(!isNavbarOpen)}
+            className={`navbar-burger${isNavbarOpen ? " open" : ""}`}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <nav
+            onClick={() => setNavbarOpen(false)}
+            className={`navbar${isNavbarOpen ? " open" : ""}`}
+          >
+            <Link to={"/"} className="navbar__item" activeClassName="active">
+              Home
+            </Link>
+            {data.allFile.edges
+              .filter(edge => {
+                return edge.node.name !== "404" && edge.node.name !== "index"
+              })
+              .map(edge => {
+                const link = `/${edge.node.name}`
+                const name = edge.node.name
+                return (
+                  <Link
+                    key={edge.node.id}
+                    to={link}
+                    className="navbar__item"
+                    activeClassName="active"
+                    partiallyActive={true}
+                  >
+                    {name}
+                  </Link>
+                )
+              })}
+          </nav>
+          <DarkSwitch
+            className="dark-switch"
+            // checked={isDarkMode}
+            // onChange={() => setDarkMode(!isDarkMode)}
+            value="darkMode"
+            inputProps={{ "aria-label": "dark mode switch" }}
+          />
+        </header>
+      )}
     />
-)
+  )
+}
 
 export default Header
