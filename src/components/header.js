d576564312b1a53b2e2e0ba127262a40445fc8e5
Sagar <sagar.jhaa@gmail.com>
2019-10-06 18:07:42 -0700

    Add header with style.

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,55 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
+import { graphql, Link, StaticQuery } from "gatsby"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+import "./header.scss"
+
+const Header = () => (
+    <StaticQuery
+        query={graphql`
+            query PageInfoQuery {
+                allFile(filter: { sourceInstanceName: { eq: "pages" } }) {
+                    edges {
+                        node {
+                            name
+                            id
+                        }
+                    }
+                }
+            }
+        `}
+      render={data => {
+        return (
+          <header>
+              <div
+                  style={{
+                      margin: `0 auto`,
+                      maxWidth: 960,
+                      padding: `1.45rem 1.0875rem`,
+                  }}
+              >
+                  <nav>
+                      {data.allFile.edges
+                          .filter(edge => {
+                              return edge.node.name !== "404" 
+                          })
+                          .map(edge => {
+                              let link = "/";
+                              let name = "Home"
+                              if (edge.node.name !== "index") {
+                                  link += edge.node.name;
+                                  name = edge.node.name.charAt(0).toUpperCase() + edge.node.name.slice(1);
+                              }
+                              return (
+                                  <Link key={edge.node.id} to={link} className="navbar-item">{name}</Link>
+                              )
+                          }
+                      )}
+                  </nav>
+              </div>
+          </header>
+      )
       }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+    />
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
