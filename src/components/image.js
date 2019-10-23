7ce15d01cd060cbc3f4ea844d9b08a04cc751e15
Sagar <sagar.jhaa@gmail.com>
2019-10-23 10:10:49 -0700

    profile image

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "profile.jpeg" }) {
+      placeholderImage: file(relativePath: { eq: "profile.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
