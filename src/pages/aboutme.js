676bb70edf12744d4b9090759996c387357f76ec
Sagar <sagar.jhaa@gmail.com>
2020-06-26 15:31:57 -0700

    TEMP

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -9,9 +9,32 @@ const AboutMe = () => (
     <SEO title="About me" />
 
     <h1>About me</h1>
-    <section>
-      <p>Welcome to About me</p>
+    <section style={{ fontSize: '1.5em' }}>
+      <div className='hand'>
+        <span style={{ fontSize: '5rem', paddingRight: '0.3em' }}>👋</span>
+      </div>
+      Hi I am Sagar.
+      I am from <a href='https://en.wikipedia.org/wiki/Gujarat' target='_blank'>Gujarat, India.</a>
+
+      <ul>
+        <li>Like to read about financial literacy</li>
+        <li>Problem solving</li>
+        <li>IOT</li>
+        <li>Reason to create the site</li>
+        <li>Reason to write blogs</li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <h3> Philosophy</h3>
+      In todays connected world, I have learned a lot from opensource projects and in general from people who are willing to share
+      what a certain concepts means to them and everybody has a way to represent the concept that appeal to people who can understand
+
+      <br />
+      <br />
+      <br />
       <Link to="/">Go back to the homepage</Link>
+
     </section>
   </Layout>
 )
